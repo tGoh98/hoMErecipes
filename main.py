380c0c56c8b380916dbc6c3f0c67b09de6c58677
tGoh98 <tmg5@rice.edu>
2020-03-28 22:32:19 -0500

    Added icons

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 # import json, createpeople, modeleval
-from flask import Flask, render_template
+from flask import Flask, render_template, json, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
-        return render_template("index.html")
+        # Submission
+        if request.method == 'POST':
+                json.dump(request.form, open('selectedIng.json', 'w'))
+                return redirect(url_for('loading'))
+
+        dairyIng = json.load(open("db/dairyIngredients.txt", "r"))
+        vegetablesIng = json.load(open("db/vegetablesIngredients.txt", "r"))
+        proteinsIng = json.load(open("db/proteinsIngredients.txt", "r"))
+        fruitsIng = json.load(open("db/fruitsIngredients.txt", "r"))
+        bakingIng = json.load(open("db/bakingIngredients.txt", "r"))
+        oilsIng = json.load(open("db/oilsIngredients.txt", "r"))
+        nutsIng = json.load(open("db/nutsIngredients.txt", "r"))
+        sugarsIng = json.load(open("db/sugarsIngredients.txt", "r"))
+
+        return render_template("index.html", dairyIng=dairyIng, vegetablesIng=vegetablesIng,
+                proteinsIng=proteinsIng, fruitsIng=fruitsIng, bakingIng=bakingIng, 
+                oilsIng=oilsIng, nutsIng=nutsIng, sugarsIng=sugarsIng)
 
 
 
